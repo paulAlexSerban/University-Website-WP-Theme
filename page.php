1d53be71d40eb9b4993494e0313c112317795b6d
Paul Serban <paul.serban@netcentric.biz>
2021-02-27 22:50:34 +0200

    UniWeb-03-THEME-added-subpages-side menu

--- page.php
+++ page.php
@@ -1,32 +1,58 @@
 <?php get_header(); ?>
 
 <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('assets/images/ocean.jpg')?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>REPLACE ME LATER</p>
-      </div>
-    </div>  
+  <div class="page-banner__bg-image"
+    style="background-image: url(<?php echo get_theme_file_uri('assets/images/ocean.jpg')?>);"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title"><?php the_title(); ?></h1>
+    <div class="page-banner__intro">
+      <p>REPLACE ME LATER</p>
+    </div>
+  </div>
+</div>
+
+<div class="container container--narrow page-section">
+
+  <?php 
+    $theParent = wp_get_post_parent_id(get_the_id());
+    if($theParent) {?>
+  <div class="metabox metabox--position-up metabox--with-home-link">
+    <p>
+      <a class="metabox__blog-home-link" href="<?php echo the_permalink($theParent);?>"><i class="fa fa-home"
+          aria-hidden="true"></i> Back to <?php echo get_the_title($theParent);?> </a>
+      <span class="metabox__main"><?php the_title(); ?></span>
+    </p>
+  </div>
+  <?php }?>
+
+<?php 
+$testArray = get_pages(array(
+  'child_of' => get_the_ID()
+));
+
+if($theParent || $testArray) { ?>
+  <div class="page-links">
+    <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent)?>"><?php echo get_the_title($theParent);?></a></h2>
+    <ul class="min-list">
+      <?php 
+      if($theParent) {
+        $findChildrenOf = $theParent;
+      } else {
+        $findChildrenOf = get_the_ID();
+      }
+      wp_list_pages(array(
+        'title_li' => NULL,
+        'child_of' => $findChildrenOf,
+        'sort_column' => 'menu_order'
+      )); ?>
+    </ul>
   </div>
 
-  <div class="container container--narrow page-section">
+<?php } ?>
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-    </div>
-    
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div> -->
-
-    <div class="generic-content">
-      <?php the_content(); ?>
-    </div>
+  <div class="generic-content">
+    <?php the_content(); ?>
+  </div>
 
 </div>
 
