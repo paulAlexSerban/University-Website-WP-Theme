fa7d121c689088b90047c3d55392b84facacc34f
Paul Serban <paul.serban@netcentric.biz>
2021-02-27 22:49:54 +0200

    UniWeb-THEME-02-added-webpack-automation

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
   function university_files() {
-      wp_enqueue_script('main_uni_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+      // wp_enqueue_script('main_uni_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
 
       wp_register_style('custom_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i', array(), '1.0');
       wp_enqueue_style( 'custom_fonts' );
@@ -8,7 +8,14 @@
       wp_register_style( 'fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css', array(), '4.2.0' );
       wp_enqueue_style( 'fontawesome' );
 
-      wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+      if(strstr($_SERVER['SERVER_NAME'], 'localhost.uniweb')) {
+        // the CSS and the JS are going to get bundled and served from this address for live development purposes
+        wp_enqueue_script('main_uni_js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+      } else {
+        wp_enqueue_script('vendors_js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
+        wp_enqueue_script('main_uni_js', get_theme_file_uri('/bundled-assets/scripts.a405fb87a4493cd04ce4.js'), NULL, '1.0', true);
+        wp_enqueue_style('university_main_styles', get_theme_file_uri('/bundled-assets/style.css'));
+      }
   };
 
   function university_features() {
