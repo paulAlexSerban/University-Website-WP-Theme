98739849b9241c0acc3549f18d2a3ac4591d8a2c
Paul Serban <paul.serban@netcentric.biz>
2021-03-12 00:15:45 +0200

    UniWeb-03-created-must-use-plugins

--- wp-content/themes/university-website/functions.php
+++ wp-content/themes/university-website/functions.php
@@ -23,79 +23,7 @@
 
   add_action('after_setup_theme', 'university_features');
 
-  function university_post_types() {
-    register_post_type('event', array(
-      'show_in_rest'=> true,
-      'supports' => array(
-        'title', 'editor', 'excerpt'
-      ),
-      'rewrite' => array(
-        'slug' => 'events'
-      ),
-      'has_archive' => true,
-      'public' => true,
-      'labels' => array(
-        'name' => 'Events',
-        'add_new_item' => 'Add New Event', 
-        'edit_item' => 'Edit Event',
-        'all_items' => 'All Events',
-        'singular_name' => 'Event'
-      ),
-      'menu_icon' => 'dashicons-calendar'
-      )
-    );
 
-    register_post_type('program', array(
-      'show_in_rest'=> true,
-      'supports' => array(
-        'title', 'editor'
-      ),
-      'rewrite' => array(
-        'slug' => 'programs'
-      ),
-      'has_archive' => true,
-      'public' => true,
-      'labels' => array(
-        'name' => 'Programs',
-        'add_new_item' => 'Add New Event', 
-        'edit_item' => 'Edit Program',
-        'all_items' => 'All Programs',
-        'singular_name' => 'Program'
-      ),
-      'menu_icon' => 'dashicons-awards'
-      )
-    );
-
-    // register_post_type('professor', array(
-    //   'has_archive' => true,
-    //   'public' => true,
-    //   'labels' => array(
-    //     'name' => 'Professors',
-    //     'add_new_item' => 'Add New Professor', 
-    //     'edit_item' => 'Edit Professor',
-    //     'all_items' => 'All Professors',
-    //     'singular_name' => 'Professor'
-    //   ),
-    //   'menu_icon' => 'dashicons-calendar-alt'
-    //   )
-    // );
-
-    // register_post_type('campus', array(
-    //   'has_archive' => true,
-    //   'public' => true,
-    //   'labels' => array(
-    //     'name' => 'Campuses',
-    //     'add_new_item' => 'Add New Campus', 
-    //     'edit_item' => 'Edit Campus',
-    //     'all_items' => 'All Campuses',
-    //     'singular_name' => 'Campuses'
-    //   ),
-    //   'menu_icon' => 'dashicons-calendar-alt'
-    //   )
-    // );
-  };
-
-  add_action('init', 'university_post_types');
 
   function university_adjust_queries($query) {
     $today = date('Ymd');
