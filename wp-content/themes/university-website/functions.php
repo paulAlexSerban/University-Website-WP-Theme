743a0a22004e9451ca280565c6a71da964069568
Paul Serban <paul.serban@netcentric.biz>
2021-03-13 01:12:06 +0200

    UniWeb-06-added-templating parts of html with functions or get-template-part

--- wp-content/themes/university-website/functions.php
+++ wp-content/themes/university-website/functions.php
@@ -1,4 +1,41 @@
 <?php
+  function pageBanner($args = NULL) {
+    /**
+     * pageBanner(array(
+     *   'title' => 'hardcoded title',
+     *   'subtitle' => 'hardcoded subtitle',
+     *   'photo' => 'hardcoded https://external-content.com'
+     * ));
+     */
+    if(!$args['title']) {
+      $args['title'] = get_the_title();
+    }
+
+    if(!$args['subtitle']) {
+      $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+
+    if(!$args['photo']) {
+      if(get_field('page_banner_-_background_image') && !is_archive() && !is_home()) {
+        $args['photo'] = get_field('page_banner_-_background_image')['sizes']['pageBanner'];
+      } else {
+        $args['photo'] = get_theme_file_uri('assets/images/ocean.jpg');
+      }
+    }
+    ?>
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);">
+  </div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
+    <div class="page-banner__intro">
+      <p><?php echo $args['subtitle'] ?></p>
+    </div>
+  </div>
+</div>
+<?php
+  }
+
   function university_files() {
     wp_register_style('custom_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i', array(), '1.0');
     wp_enqueue_style( 'custom_fonts' );
--- wp-content/themes/university-website/page.php
+++ wp-content/themes/university-website/page.php
@@ -1,15 +1,9 @@
-<?php get_header(); ?>
-
-<div class="page-banner">
-  <div class="page-banner__bg-image"
-    style="background-image: url(<?php echo get_theme_file_uri('assets/images/ocean.jpg')?>);"></div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"><?php the_title(); ?></h1>
-    <div class="page-banner__intro">
-      <p>REPLACE ME LATER</p>
-    </div>
-  </div>
-</div>
+<?php get_header(); 
+pageBanner(array(
+  'title' => 'example title',
+  'subtitle' => 'example subtitle',
+  'photo' => 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.henryford.com%2F-%2Fmedia%2Fhenry-ford-blog%2Fimages%2Finterior-banner-images%2F2019%2F11%2Ftest-anxiety.jpg&f=1&nofb=1'
+));?>
 
 <div class="container container--narrow page-section">
 
